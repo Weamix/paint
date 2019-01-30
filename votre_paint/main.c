edf31800b4d9113af9dd1e4406c11660a88cf399
Weamix <weamix16@gmail.com>
2019-01-30 15:21:43 +0100

    Add files via upload

--- votre_paint/main.c
+++ votre_paint/main.c
@@ -9,8 +9,8 @@ int main(int argc, char** argv)
   creation_fenetre(COIN_HAUT_GAUCHE_X,COIN_HAUT_GAUCHE_Y,TAILLE_X,TAILLE_Y);
 
   printf("Appuyez sur les touches du pavé numérique de 1 à 4 pour voir des points apparaître\n");
-  printf("Appuyer sur la touche 5 du clavier pour creer un cercle");
-  printf("Appuyer sur la touche 9 du clavier pour creer un rectangle");
+  printf("Appuyer sur la touche 5 du clavier pour creer un cercle\n");
+  printf("Appuyer sur la touche 9 du clavier pour creer un rectangle\n");
   printf("Appuyez sur la touche 6 pour entrer les coordonnées d'un point à afficher\n");
   printf("Appuyez sur la touche 7 pour reinitialiser la fenêtre\n");
   printf("Appuyez sur la touche 8 pour tout effacer\n");
