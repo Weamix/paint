2b571be85515bdc9d7c12abbd02d0f643cf70102
Charles <47041967+SpartanCharles@users.noreply.github.com>
2019-03-06 17:51:59 +0100

    Finish Update.

--- votre_paint/main.c
+++ votre_paint/main.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
 int main(int argc, char** argv)
-{
+ {
   creation_fenetre(COIN_HAUT_GAUCHE_X,COIN_HAUT_GAUCHE_Y,TAILLE_X,TAILLE_Y);
 
   printf("Appuyez sur les touches du pavé numérique de 1 à 3 pour voir des points apparaître\n");
@@ -16,6 +14,8 @@ int main(int argc, char** argv)
   printf("Appuyez sur la touche 7 pour reinitialiser la fenêtre\n");
   printf("Appuyez sur la touche 8 pour tout effacer\n");
   printf("Ou cliquez dans la fenêtre avec la souris\n");
+  
   lancer_application();
+  
   return 0;
-}
+ }
