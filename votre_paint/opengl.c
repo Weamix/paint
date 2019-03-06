9fecbe19f9dd497a9e3454f7ac707eaa0303cecc
Charles <47041967+SpartanCharles@users.noreply.github.com>
2019-03-06 17:55:06 +0100

    Finish Update.

--- votre_paint/opengl.c
+++ votre_paint/opengl.c
@@ -5,68 +5,70 @@
 int premierAffichage=0;
 
 void lancer_application()
-{
+ {
   glutMainLoop();
-}
+ }
 
 void rafraichir()
-{
+ {
   glutPostRedisplay();
-}
+ }
 
-void effacer(){
+void effacer()
+ {
   glClear (GL_COLOR_BUFFER_BIT);
   glLoadIdentity();
-}
+ }
 
 void texte(int x, int y, char *string)
-{
-	int len,i; // len donne la longueur de la chaîne de caractères
+ {
+  int len,i; // "len" donne la longueur de la chaîne de caractères.
 
-	glRasterPos2f(x,y); // Positionne le premier caractère de la chaîne
-	len = (int) strlen(string); // Calcule la longueur de la chaîne
-	for (i = 0; i < len; i++) glutBitmapCharacter(GLUT_BITMAP_9_BY_15,string[i]); // Affiche chaque caractère de la chaîne
-}
-
+  glRasterPos2f(x,y); // Positionne le premier caractère de la chaîne.
+  len = (int) strlen(string); // Calcule la longueur de la chaîne.
+  for (i = 0; i < len; i++) glutBitmapCharacter(GLUT_BITMAP_9_BY_15,string[i]); // Affiche chaque caractère de la chaîne.
+ }
 
 void display()
-{
-  if(premierAffichage==0){
+ {
+  if(premierAffichage==0)
+   {
     effacer();
     initialisation_fenetre();
     premierAffichage=1;
-  }	
+   }	
   affichage();
   
   glFlush();
   glutSwapBuffers();
-}
+ }
 
 void foncSouris(int button, int state, int x, int y)
-{
-  switch (button){
-  case GLUT_LEFT_BUTTON :
-    if (state==GLUT_UP) clic_gauche(x,y);
-    break;
+ {
+  switch (button)
+   {
+    case GLUT_LEFT_BUTTON :
+    			if (state==GLUT_UP) clic_gauche(x,y);
+    			break;
     
-  case GLUT_RIGHT_BUTTON :
-    if (state==GLUT_UP) clic_droit(x,y);
-    break;
-  }
-}
+    case GLUT_RIGHT_BUTTON :
+    			if (state==GLUT_UP) clic_droit(x,y);
+    			break;
+   }
+ }
 
 void foncClavier(unsigned char key, int x, int y)
-{
+ {
   clavier(key,x,y);
-}
+ }
 
 void attente()
-{
+ {
   rafraichir();
-}
+ }
 
 void reshape (int w, int h)
-{
+ {
   w=h=0;
   glViewport (0, 0, TAILLE_X, TAILLE_Y); 
   glMatrixMode (GL_PROJECTION);
@@ -74,12 +76,12 @@ void reshape (int w, int h)
   gluOrtho2D(0,TAILLE_X,0,TAILLE_Y);
   glMatrixMode(GL_MODELVIEW);
   rafraichir();
-  }
-
+ }
 
 void creation_fenetre(int posX, int posY, int tailleX, int tailleY) 
-{
+ {
   int var=0;
+	
   glutInit(&var, NULL);
   glutInitDisplayMode (GLUT_RGB);
   glutInitWindowSize (tailleX, tailleY); 
@@ -95,4 +97,4 @@ void creation_fenetre(int posX, int posY, int tailleX, int tailleY)
   glutKeyboardFunc(foncClavier);
 
   glClear (GL_COLOR_BUFFER_BIT);
-}
+ }
