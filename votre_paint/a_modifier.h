87550741b7a4b3fcd380406cd4ca3dba112eefaa
Charles <47041967+SpartanCharles@users.noreply.github.com>
2019-03-09 10:29:05 +0100

    Update a_modifier.h

--- votre_paint/a_modifier.h
+++ votre_paint/a_modifier.h
@@ -10,5 +10,11 @@ void affichage();
 void clic_gauche(int x, int y);
 void clic_droit(int x, int y);
 void clavier(unsigned char key, int x, int y);
+void insertion(ptcercle *sauvegarde, ptcercle cercle);
+void affichersauvegarde(ptcercle sauvegarde);
+void insertion(ptdroite *liste_de_droite, ptdroite droite);
+void afficherListe(ptdroite liste_de_droite);
+void insertion(ptrectangle *sauvegarde, ptrectangle rectangle);
+void affichersauvegarde(ptrectangle sauvegarde);
 
 #endif
